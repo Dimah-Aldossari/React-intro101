404a7aa192fd2638c6b5601b98b175d971abc84a
Masha <m.aldosary@safcsp.org.sa>
2022-09-19 13:51:47 +0300

    react intro101

---
+++ src/component/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <div>
+        
+        <ul className='list'>
+            <li>Home</li>
+            <li>about</li>
+
+        </ul>
+    </div>
+  )
+}
+
+
+export default Nav;
